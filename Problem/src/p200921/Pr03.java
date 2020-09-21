65c662bea22c4ec770014109f07144e2c0cd04e0
GDJ30 <GDJ30@192.168.0.136>
2020-09-21 18:21:42 +0900

    UPDATE Pr03 과제 풀이 수정 변수 -> 배열로 수정

--- Problem/src/p200921/Pr03.java
+++ Problem/src/p200921/Pr03.java
@@ -50,42 +50,16 @@ package p200921;
 import java.util.Scanner;
 
 class Wallet {
-	int krw500;
-	int krw100;
-	int krw50;
-	int krw10;
-	int krw5;
-	int krw1;
-	int sum;
-	
-	void sum() {
-		sum = krw500 * 500 + krw100 * 100 + krw50 * 50 + krw10 * 10 + krw5 * 5 + krw1 * 1;
-	}
+	final int[] KRW = {500, 100, 50, 10, 5, 1};
+
+	int[] coin = new int[6];
 	
 	Boolean calc(int price) {
-		int tmp = (price/500 > krw500) ? krw500 : price/500;
-		price -= tmp * 500;
-		krw500 -= tmp;
-		
-		tmp = (price/100 > krw100) ? krw100 : price/100;
-		price -= tmp * 100;
-		krw100 -= tmp;
-		
-		tmp = (price/50 > krw50) ? krw50 : price/50;
-		price -= tmp * 50;
-		krw50 -= tmp;
-		
-		tmp = (price/10 > krw10) ? krw10 : price/10;
-		price -= tmp * 10;
-		krw10 -= tmp;
-		
-		tmp = (price/5 > krw5) ? krw5 : price/5;
-		price -= tmp * 5;
-		krw5 -= tmp;
-		
-		tmp = (price/1 > krw1) ? krw1 : price/1;
-		price -= tmp * 1;
-		krw1 -= tmp;
+		for(int i = 0; i < 6; i++) {
+			int tmp = (price/KRW[i] > coin[i]) ? coin[i] : price/KRW[i];
+			price -= tmp * KRW[i];
+			coin[i] -= tmp;
+		}
 		
 		if(price != 0) {
 			return false;
@@ -103,32 +77,23 @@ public class Pr03 {
 		final int COUNT = 5;
 		
 		Wallet w = new Wallet();
-		w.krw500 = COUNT;
-		w.krw100 = COUNT;
-		w.krw50 = COUNT;
-		w.krw10 = COUNT;
-		w.krw5 = COUNT;
-		w.krw1 = COUNT;
+		for(int i = 0; i < 6; i++) {
+			w.coin[i] = COUNT;
+		}
 		System.out.println();
 		
 		if(!w.calc(price)) {
 			System.out.println("금액이 부족합니다.");
 			return;
 		} else {
-			System.out.println("500원: " + (COUNT - w.krw500) + "개");
-			System.out.println("100원: " + (COUNT - w.krw100) + "개");
-			System.out.println("50원: " + (COUNT - w.krw50) + "개");
-			System.out.println("10원: " + (COUNT - w.krw10) + "개");
-			System.out.println("5원: " + (COUNT - w.krw5) + "개");
-			System.out.println("1원: " + (COUNT - w.krw1) + "개");
+			for(int i = 0; i < 6; i++) {
+				System.out.printf("%d원: %d개\n", w.KRW[i], COUNT - w.coin[i]);
+			}
 		}
 		System.out.println();
 		
-		System.out.println("남은 동전 500원: " + w.krw500 + "개");
-		System.out.println("남은 동전 100원: " + w.krw100 + "개");
-		System.out.println("남은 동전 50원: " + w.krw50 + "개");
-		System.out.println("남은 동전 10원: " + w.krw10 + "개");
-		System.out.println("남은 동전 5원: " + w.krw5 + "개");
-		System.out.println("남은 동전 1원: " + w.krw1 + "개");
+		for(int i = 0; i < 6; i++) {
+			System.out.printf("남은 동전 %d원: %d개\n", w.KRW[i], w.coin[i]);
+		}
 	}
 }
